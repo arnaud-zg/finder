d464da1e9d46d82ee24e5098a9ff79ea11450181
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-28 23:27:20 +0200

    chore(project): add csx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { style } from 'typestyle'
+import { rem } from 'csx'
 import { Navigation } from '../Navigation'
 
 const className = style({
-  padding: '0 32px',
+  padding: `0 ${rem(1.5)}`,
 })
 
 export class Layout extends React.PureComponent {
---
+++ constants/colors.ts
@@ -0,0 +1,11 @@
+import { color } from 'csx'
+
+const dark = color('#333333')
+const light = color('#FFFFFF')
+const primary = color('#067df7')
+
+export const colors = {
+  dark,
+  light,
+  primary,
+}
