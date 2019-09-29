82bc81d0a0de879c43b697f4cd5e54b656abbfdb
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-29 14:25:19 +0200

    fix(announcement): add description on announcement page

--- pages/announcement.tsx
+++ pages/announcement.tsx
@@ -1,23 +1,49 @@
 import Head from 'next/head'
 import React from 'react'
 import { Layout } from '../components/Layout'
+import { Card } from '../components/Card'
+import { Paragraph } from '../components/Paragraph'
 
-export default class AnnouncementPage extends React.PureComponent {
+interface IProps {
+  page: {
+    description: string
+    disambiguatingDescription: string
+    identifier: string
+  }
+}
+
+export default class AnnouncementPage extends React.PureComponent<IProps> {
   static async getInitialProps() {
     await new Promise(resolve => {
       setTimeout(resolve, 500)
     })
 
-    return { data: {} }
+    return {
+      page: {
+        description:
+          'Find the most recent announcements with the best opportunities. Stay connected to take advantage of our announcements.',
+        disambiguatingDescription:
+          'Find the most recent announcements with the best opportunities by the themes that we offer: real estate offer, sale opportunity. Stay connected to take advantage of our announcements.',
+        identifier: 'announcement',
+      },
+    }
   }
 
   render() {
+    const { page } = this.props
+    const { description } = page
+
     return (
       <Layout>
         <Head>
           <title>Announcement | Finder</title>
         </Head>
-        <p>Content for Announcement page</p>
+
+        {!!description && (
+          <Card>
+            <Paragraph content={description} />
+          </Card>
+        )}
       </Layout>
     )
   }
