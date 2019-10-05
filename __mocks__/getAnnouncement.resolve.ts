5ce6b06d2957d1c8522ba716b1e0b0023643994f
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-10-06 00:29:44 +0200

    fix(annoncement): add visibility property on announcement mock (#55)

--- __mocks__/getAnnouncement.resolve.ts
+++ __mocks__/getAnnouncement.resolve.ts
@@ -30,30 +30,33 @@ export const payload = {
     '123': {
       list: [
         {
+          description: 'Ground floor Studio apartment with Balcony !',
           identifier: '1',
           locationIndication: 'Paris Brochant - Rue des Moines',
-          description: 'Ground floor Studio apartment with Balcony !',
           price: 75000,
           publishTime: '2019-09-29T13:50:42.926Z',
           type: 'apartment',
+          visibility: 'public',
           // https://paris.craigslist.org/apa/d/ground-floor-studio-apartment-with/6988874946.html
         },
         {
           courseDuration: '2h',
-          identifier: '2',
           description:
             "Improve your Online Shopify Store with France's Top Shopify Expert",
+          identifier: '2',
           price: 9700,
           publishTime: '2019-09-29T13:50:42.926Z',
           type: 'learning',
+          visibility: 'public',
           // https://paris.craigslist.org/syd/d/improve-your-online-shopify-store-with/6986077334.html
         },
         {
+          description: 'Native English Speaker Seeking Accomodation',
           identifier: '3',
           locationIndication: 'Montmartre - Paris',
-          description: 'Native English Speaker Seeking Accomodation',
           publishTime: '2019-09-29T13:50:42.926Z',
           type: 'request',
+          visibility: 'public',
           // https://paris.craigslist.org/hss/d/native-english-speaker-seeking/6984153650.html
         },
       ],
