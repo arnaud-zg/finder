3149a4971d401a09fc46878f5ee614d494542c79
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-10-06 00:15:04 +0200

    fix(input): support input number and update select style (#48)

---
+++ components/NumberInput/NumberInput.tsx
@@ -0,0 +1,41 @@
+import { percent, rem } from 'csx'
+import { Field } from 'formik'
+import React from 'react'
+import { style } from 'typestyle'
+import { colors } from '../../constants/colors'
+import { ErrorField } from '../ErrorField'
+
+const INPUT_TYPE_NUMBER = 'number'
+
+const className = style({
+  borderColor: colors.gray.toHexString(),
+  borderRadius: rem(0.25),
+  borderStyle: 'solid',
+  borderWidth: rem(0.0625),
+  fontSize: rem(1),
+  marginTop: rem(0.5),
+  padding: rem(0.5),
+  width: percent(100),
+})
+
+interface IProps extends React.HTMLProps<HTMLInputElement> {}
+
+export class NumberInput extends React.PureComponent<IProps> {
+  render() {
+    return (
+      <Field name={this.props.name}>
+        {({ field }) => (
+          <>
+            <input
+              {...field}
+              {...this.props}
+              className={className}
+              type={INPUT_TYPE_NUMBER}
+            />
+            <ErrorField fieldName={field.name} />
+          </>
+        )}
+      </Field>
+    )
+  }
+}
