0fc66e1e339fbccf9402ddef8393161512ea6cdf
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-10-07 15:27:33 +0200

    fix(item): hide private item content

--- services/announcementCreate.ts
+++ services/announcementCreate.ts
@@ -12,15 +12,27 @@ export const addAnnouncement = async (payload): Promise<IAnnouncement> =>
         type,
         visibility,
       } = payload
-      resolve({
-        description,
-        identifier: new Date().toISOString(),
-        locationIndication,
-        price,
-        publishTime: new Date().toISOString(),
-        type,
-        visibility,
-      })
+      if (visibility === 'private') {
+        resolve({
+          description: '------',
+          identifier: new Date().toISOString(),
+          locationIndication: null,
+          price: 0,
+          publishTime: new Date().toISOString(),
+          type,
+          visibility,
+        })
+      } else {
+        resolve({
+          description,
+          identifier: new Date().toISOString(),
+          locationIndication,
+          price,
+          publishTime: new Date().toISOString(),
+          type,
+          visibility,
+        })
+      }
       NProgress.done()
     }, RESPONSE_DURATION_IN_MS)
   })
